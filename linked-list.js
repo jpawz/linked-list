db11588b7d66644378a7123e30816d0486740091
Paweł J <jpawz@outlook.com>
2023-04-06 21:27:35 +0200

    Implement prepend and append functions

--- linked-list.js
+++ linked-list.js
@@ -2,4 +2,22 @@ class LinkedList {
   constructor(head = null) {
     this.head = head;
   }
+
+  append(newNode) {
+    if (this.head == null) {
+      this.head = newNode;
+      return;
+    }
+    let node = this.head;
+    while (node.next) {
+      node = node.next;
+    }
+    node.next = newNode;
+  }
+
+  prepend(newNode) {
+    const node = this.head;
+    this.head = newNode;
+    this.head.next = node;
+  }
 }
